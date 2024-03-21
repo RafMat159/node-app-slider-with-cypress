4da4d3fc4b02f8848cee1ee799d7299f9aa72703
Rafał Matusiak <rmatusiak159@gmail.com>
2024-03-21 19:16:25 +0100

    new changes

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -1,6 +1,6 @@
 describe('Swiper Gallery Test', function () {
   it('Checks if second slide contains "United Kingdom"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
   });
@@ -8,11 +8,24 @@ describe('Swiper Gallery Test', function () {
 
 describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
-    cy.visit('http://app:3000');
+    cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
     cy.wait(2000)
     cy.get('.swiper-button-next').click({ force: true });
     cy.wait(2000)
     cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if "', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000)
+    cy.get('.swiper-slide-active').should('contain', 'London');
+    cy.wait(2000)
+    cy.get('.swiper-button-prev').click();
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.wait(2000)
   });
 });
